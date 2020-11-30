2eb2410bd1089a894adf50ad0151585da8570122
tonyxli <xiuqi1028tl@gmail.com>
2020-11-30 03:10:32 -0700

    javadoc

--- Booksies/app/src/main/java/com/example/booksies/model/notification/Notification.java
+++ Booksies/app/src/main/java/com/example/booksies/model/notification/Notification.java
@@ -1,3 +1,7 @@
+/*
+*
+ */
+
 package com.example.booksies.model.notification;
 /**
  * Configure a notification
@@ -6,23 +10,44 @@ public class Notification implements notifiable {
     public String title;
     public String body;
 
+    /**
+     * Constructor for Notification class
+     * @param title: String
+     * @param body: String
+     */
     public Notification(String title, String body) {
         this.title = title;
         this.body = body;
     }
 
+    /**
+     * Get title of Notification
+     * @return String
+     */
     public String getTitle() {
         return title;
     }
 
+    /**
+     * Set title of Notification
+     * @param title: String
+     */
     public void setTitle(String title) {
         this.title = title;
     }
 
+    /**
+     * Get body of Notification
+     * @return
+     */
     public String getBody() {
         return body;
     }
 
+    /**
+     * Set body of Notification
+     * @param body: String
+     */
     public void setBody(String body) {
         this.body = body;
     }
